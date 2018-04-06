44e893359f9b922a7768b3c45d2db0558ebb54ca
afpatmin <patrick.minogue@appfoundry.se>
2018-04-06 12:38:29 +0200

    publish

---
+++ lib/fo_model.dart
@@ -0,0 +1,12 @@
+library fo_model;
+
+class FoModel {
+  FoModel(this.id);
+
+ // Map<String, dynamic> toJson() => {'id':id.toString()};
+
+  @override
+  String toString() => id.toString();
+
+  final Object id;
+}
