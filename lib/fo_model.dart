45e9d1de2b886f341c1903a038f30d698843d392
afpatmin <patrick.minogue@appfoundry.se>
2018-04-06 14:33:53 +0200

    made id not final

--- lib/fo_model.dart
+++ lib/fo_model.dart
@@ -1,12 +1,12 @@
 library fo_model;
 
 class FoModel {
-  FoModel(this.id);
+  FoModel([this.id = null]);
 
   Map<String, dynamic> toJson() => {'id':id.toString()};
 
   @override
   String toString() => id.toString();
 
-  final Object id;
+  Object id;
 }
